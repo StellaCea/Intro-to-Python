bb022b9a2d45978d374769b0ae27b22743c1a49a
StellaCea <stela.ceaicovscaia@hotmail.com>
2023-09-09 21:13:09 +0200

    solved ex.2.3

---
+++ Exercise-2/exercise-2.3/src/users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=120, unique=True)
+    name = models.CharField(max_length=120)
+    password = models.CharField(max_length=120)
+    bio = models.TextField()
+
+    def __str__(self):
+        return str(self.username)
+    
+    
